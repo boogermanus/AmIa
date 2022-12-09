0976c979945d301250e606dde94915a72d3c30b9
jordan.woodruff <jordan.woodruff@tylertech.com>
2022-12-08 21:35:35 -0600

    remove unused imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,4 @@
 import { Component } from '@angular/core';
-import {SectionService} from "./services/section.service";
-import {SkillLevelService} from "./services/skillLevel.service";
-import {ISection} from "./interfaces/isection";
-import { ISkillLevel } from './interfaces/iskillLevel';
-import { NumberFormatStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
