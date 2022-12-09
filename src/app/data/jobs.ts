3c26c590fbcaa57bce93bd5a568e5b4407d2ed1f
jordan.woodruff <jordan.woodruff@tylertech.com>
2022-12-08 18:21:35 -0600

    add jobs

---
+++ src/app/data/jobs.ts
@@ -0,0 +1,30 @@
+﻿import {IJob} from "../interfaces/ijob";
+
+const jobs: IJob[] = [
+  {
+    name: "Developer Intern",
+    requiredScore: 0
+  },
+  {
+    name: "Software Developer",
+    requiredScore: 0
+  },
+  {
+    name: "Software Engineer",
+    requiredScore: 0
+  },
+  {
+    name: "Senior Software Engineer",
+    requiredScore: 0
+  },
+  {
+    name: "Lead Engineer",
+    requiredScore: 0
+  },
+  {
+    name: "Senior Lead Engineer",
+    requiredScore: 0
+  }
+];
+
+export {jobs};
