22385f983a15a564f9b2c2a2ff4deb65907e06a6
jordan.woodruff <jordan.woodruff@tylertech.com>
2022-12-26 18:35:59 -0600

    additional testing

--- src/app/components/rating/rating.component.spec.ts
+++ src/app/components/rating/rating.component.spec.ts
@@ -1,16 +1,21 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { RatingComponent } from './rating.component';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+import {RatingComponent} from './rating.component';
+import {ExpectedCompetencyService} from "../../services/expectedCompetency.service";
 
 describe('RatingComponent', () => {
   let component: RatingComponent;
   let fixture: ComponentFixture<RatingComponent>;
+  let expectedCompetencySpy: jasmine.SpyObj<ExpectedCompetencyService>;
 
   beforeEach(async () => {
+    const spy = jasmine.createSpyObj('expectedCompetencyService',['getExpectedScoreByJobId']);
     await TestBed.configureTestingModule({
-      declarations: [ RatingComponent ]
+      declarations: [RatingComponent],
+      providers: [{provide: ExpectedCompetencyService, useValue: spy}]
     })
-    .compileComponents();
+      .compileComponents();
+
+    expectedCompetencySpy = TestBed.inject(ExpectedCompetencyService) as jasmine.SpyObj<ExpectedCompetencyService>;
 
     fixture = TestBed.createComponent(RatingComponent);
     component = fixture.componentInstance;
@@ -19,5 +24,37 @@ describe('RatingComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('onRatingChanged', () => {
+    it('should add to totalScore', () => {
+      const expected = {previousValue: 0, newValue: 1}
+      component.onRatingChanged(expected);
+      expect(component.totalScore).toEqual(expected.newValue)
+    });
+
+    it('should set totalScore to newValue', () => {
+      component.totalScore = 1;
+      const expected = {previousValue: 2, newValue: 1};
+      component.onRatingChanged(expected);
+      expect(component.totalScore).toEqual(expected.newValue);
+    });
+  });
+  describe('jobId', () => {
+    it('should set totalScore to zero', () =>{
+      component.totalScore = 10;
+      component.jobId = 0;
+      expect(component.totalScore).toEqual(0);
+    });
+    it('should call expected competency service', () => {
+      component.jobId = 0;
+      expect(expectedCompetencySpy.getExpectedScoreByJobId).toHaveBeenCalled();
+    });
+    it('should set neededScore equal to 1', () => {
+      component.neededScore = 10;
+      expectedCompetencySpy.getExpectedScoreByJobId.and.returnValue(1);
+      component.jobId = 0;
+      expect(component.neededScore).toEqual(1);
+    })
   });
 });
